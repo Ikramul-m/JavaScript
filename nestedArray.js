36e43688253e31d22640ca1e2db63eef02fd9913
Ikramul-m <i20727172@gmail.com>
2024-03-24 21:52:07 +0530

    lesson

---
+++ nestedArray.js
@@ -0,0 +1,14 @@
+var myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "brich"],
+  },
+];
+
+var my = myPlants[1].list[1];
+console.log(my);
+console.log(myPlants[0].type);
