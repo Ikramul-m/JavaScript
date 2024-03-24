d6d59cdee001c101485db33519aba78fcce9ef9d
Ikramul-m <i20727172@gmail.com>
2024-03-24 19:13:29 +0530

    lesson

--- Lookup.js
+++ Lookup.js
@@ -7,6 +7,7 @@ function phoneticLookup(val) {
     language: "C#",
     age: "24",
     dob: "26/01/2000",
+    placeofBirth: "Katigorah",
   };
 
   result = lookup[val];
@@ -14,8 +15,15 @@ function phoneticLookup(val) {
   return result;
 }
 
-console.log(phoneticLookup("name"));
+console.log("He is " + phoneticLookup("name"));
 console.log("Is a " + phoneticLookup("profession") + ".");
 console.log("Expertise in " + phoneticLookup("language") + ".");
 console.log("Is " + phoneticLookup("age") + " years old" + ".");
-console.log("Was born in " + phoneticLookup("dob") + ".");
+
+console.log(
+  "Was born in " +
+    phoneticLookup("placeofBirth") +
+    " on " +
+    phoneticLookup("dob") +
+    "."
+);
