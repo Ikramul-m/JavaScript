2d955186fc9217dc9e8d31d2ae1fefff94287cd6
Ikramul-m <i20727172@gmail.com>
2024-03-25 10:24:27 +0530

    Lesson

--- loop.js
+++ loop.js
@@ -1,8 +1,11 @@
 var myArray = [];
 
-var i = 0;
-while (i < 5) {
-  myArray.push(i++);
-}
+// var i = 0;
+// while (i < 5) {
+//   myArray.push(i++);
+// }
 
+// console.log(myArray);
+
+for (var i = 0; i < 6; myArray.push(i++)) {}
 console.log(myArray);
