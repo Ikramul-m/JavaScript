0c85a09d857770aebba84e14c0e5db3959dd9347
Ikramul-m <i20727172@gmail.com>
2024-03-25 21:44:51 +0530

    lesson

--- destructing.js
+++ destructing.js
@@ -1,10 +1,10 @@
-var voxel = { x: 3.6, y: 7.4, z: 6.54 };
+// var voxel = { x: 3.6, y: 7.4, z: 6.54 };
 
-var x = voxel.x;
-var y = voxel.y;
-var z = voxel.z;
+// var x = voxel.x;
+// var y = voxel.y;
+// var z = voxel.z;
 
-const { x: a, y: b, z: c } = voxel;
+// const { x: a, y: b, z: c } = voxel;
 
 const AVG_TEMPERATURES = {
   today: 77.5,
